112327f3f9b2ba3a90fa831ec64bb190468378f3
Alexey Mytsykov <alexmyt1@gmail.com>
2023-09-13 13:57:51 +0300

    feat: use .env for configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AmoCRMModule } from './modules/amocrm/amocrm.module';
 import { ContactsModule } from './modules/contacts/contacts.module';
 import { LeadsModule } from './modules/leads/leads.module';
 
 @Module({
-  imports: [AmoCRMModule, ContactsModule, LeadsModule],
+  imports: [AmoCRMModule, ContactsModule, LeadsModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
